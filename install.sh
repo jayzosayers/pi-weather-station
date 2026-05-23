d6a91e6891c4413471f7918eb6bbeca2eb030dfa
Jayy <me@jamie-sayers.uk>
2026-05-23 20:14:05 +0100

    package issue fix

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ apt-get install -y \
   python3 python3-venv python3-pip python3-dev \
   build-essential \
   i2c-tools spi-tools gpiod \
-  python3-gpiozero python3-smbus python3-smbus2 \
+  python3-gpiozero \
+  python3-smbus python3-smbus2 \
   python3-serial \
-  libgpiod2 pigpio python3-pigpio
+  libgpiod-dev \
+  python3-libgpiod
 
 python3 -m venv "$VENV_DIR"
 "$VENV_DIR/bin/pip" install --upgrade pip wheel setuptools
